9a7b03e997abbe4a9517da5e9039703a832485c9
christian <christianjones94@gmail.com>
2022-10-16 19:46:02 -0400

    set DOM to variables

---
+++ index.js
@@ -0,0 +1,7 @@
+let result = document.getElementById("result");
+let btn = document.getElementById("calculate-btn");
+let input = document.getElementById("input-txt");
+
+btn.addEventListener("click", function () {
+  result.innerHTML = 5;
+});
